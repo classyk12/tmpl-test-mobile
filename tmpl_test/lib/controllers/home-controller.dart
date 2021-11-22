0cafbc110b4590601c04349cd8b0f843d8105393
Faith SODIPE <faithsodipe@gmail.com>
2021-11-22 16:24:59 +0100

    added view/controller

---
+++ tmpl_test/lib/controllers/home-controller.dart
@@ -0,0 +1,35 @@
+import 'package:get/get.dart';
+import 'package:tmpl_test/models/jokes_model.dart';
+import '';
+
+class HomeController extends GetxController {
+  var progress = LoadingEnum.loading.obs;
+  var jokes = <JokeModel>[].obs;
+  var error = ''.obs;
+  var jokeService = JokesService();
+
+  @override
+  void onInit() {
+    super.onInit();
+    getData();
+  }
+
+  Future getData() async {
+    progress.value = LoadingEnum.loading;
+    try {
+      var response = await jokeService.getJokes();
+      progress.value = LoadingEnum.done;
+      if (response != null) {
+        jokes.assignAll(response.data);
+      } else {
+        progress.value = LoadingEnum.failed;
+      }
+    } catch (err) {
+      progress.value = LoadingEnum.failed;
+      error.value = err.toString();
+      print('err');
+    }
+  }
+}
+
+enum LoadingEnum { loading, done, failed }
